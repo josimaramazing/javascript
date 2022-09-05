e30dea10fb7ae76a5160e39f40eee802e8cf57b3
josimar <josimarsofive@gmail.com>
2022-09-05 17:00:23 -0500

    add variables.js

---
+++ variables.js
@@ -0,0 +1,56 @@
+/*Declaracion de variables*/
+/*Declaracion de variables = tipos string, boolean, number y symbol*/
+var miNombre = "Josimar";
+let miApellido ='Leon';
+const nickName = "Cachorro";
+
+/*let es una variable con mas alcance*/
+let number = 2;
+let boolean = true; //false
+
+/*Las variables constantes no puede cambiar su valor*/
+const name = 'Jota';
+const pi = 3.14;
+//name = 'jeffer';
+//pi=34;
+
+/*valor undefined existe pero no tiene valor, en conclusion no tiene valor*/
+
+let carro;
+
+/*scope*/
+
+let number1, number2, number3;
+number1 = 2;
+number2 = 3;
+number3 = 5;
+
+/*hoisting*/
+
+let marcaGamer;
+
+/*null*/
+
+let marca = null;
+
+/*NaN*/
+let numero1 = 4;
+let numero2 = 8;
+let dato = 'gamer';
+/*prompt*/
+let nombre = prompt('Hola eres gamer?');
+//alert('Hola '+ nombre);
+
+
+
+/*debug*/
+alert(numero1 * dato);
+//prompt()
+//alert(numero1 * numero2);
+
+marcaGamer ='razer';
+
+/*comentarios*/
+/*let y var si pueden cambiar de valor*/
+/*const siempre se inicializa y no se puede modificar*/
+
