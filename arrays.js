45272767dee4a28b95bc8ebda4100a8f589953d4
josimar <josimarsofive@gmail.com>
2022-09-05 21:15:51 -0500

    add arrays.js

---
+++ arrays.js
@@ -0,0 +1,25 @@
+/* arrays */
+let frutas = ['banana','fruta','pera'];
+document.write(frutas[0]);
+
+/* arrays asociativo*/
+
+let pc ={
+    nombre:'JOTA',
+    procesador: 'M1',
+    ram: '16GB',
+    espacio: '526gb'
+};
+
+let nombre = pc['nombre'];
+let procesador = pc['procesador'];
+let ram = pc['ram'];
+let espacio = pc['espacio'];
+
+frase =`el nombre de mi pc es ${nombre} <br>
+        el procesador es: ${procesador} <br>
+        la memoria ram es: ${ram} <br>
+        el espacio de disco es: ${espacio}
+ `;
+
+ document.write(frase);
