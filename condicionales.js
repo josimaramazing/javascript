e70d9e57c1f6d0efdd82179c4bbdce5e62bea992
josimar <josimarsofive@gmail.com>
2022-09-05 20:54:49 -0500

    add condicionales.js

---
+++ condicionales.js
@@ -0,0 +1,19 @@
+/*Condicionales*/ 
+
+{
+    alert('Hola Gente');
+    document.write('soy josimar')
+}
+
+/*Condicionales - ifelse : si la condicion es true el bloque se ejecuta*/
+
+let nombre = 'Pedro';
+
+if (nombre == 'lucky') {
+    alert(`tu nombre es buenardo ${nombre}`)
+} else if(nombre == 'Pepe'){
+    alert(`tu nombre es malardo ${nombre}`)
+}else{
+    alert(`tu nombre es otro`)
+}
+
