9afe2e377c9ecc66fbe62f5fffabae287d9365e6
josimar <josimarsofive@gmail.com>
2022-09-09 11:01:44 -0500

    modified code and function the operators.js

--- operadores.js
+++ operadores.js
@@ -1,61 +1,97 @@
-/*Operadores aritmeticos*/
-/*Asignacion*/
+/**
+ * * Operadores
+ */
 let numero = 15;
-/*uso =, -, *, %, **, */
-//numero **= 5;
+let numero10 = 10;
 
+/**
+ * * Operadores uso =, -, *, % y **
+ */
+function exerciseOne() {
+  numero -= 1;
+  document.write(numero + "<br>");
+  numero += 1;
+  document.write(numero + "<br>");
+  numero *= 2;
+  document.write(numero + "<br>");
+  numero %= 1;
+  document.write(numero + "<br>");
+  numero ** 2;
+  document.write(numero + "<br>");
+}
+exerciseOne();
+
+function exerciseTwo() {
+  numero10--;
+  document.write(numero10 + "<br>");
+  numero10++;
+  document.write(numero10 + "<br>");
+}
+exerciseTwo();
 /*operadores*/
-numero--;
-numero++;
 
-//resultado = numero;
-resultado = numero**2;
-resultado = numero++;
+function exerciseThree() {
+  let result = numero10 ** 2;
+  document.write(result + "<br>");
+  let result2 = numero10++;
+  document.write(result2 + "<br>");
+}
+exerciseThree();
 
 /*concatenacion*/
+function exerciseFour() {
+  let saludo = "!Bienvenida! ";
+  let question = "Como estas?";
 
-let bienvenida = '!Bienvenida!'
-let question = 'Como estas?'
-
-resultado = bienvenida+question;
-
+  let resultado = saludo + question;
+  document.write(resultado + "<br>");
+}
+exerciseFour();
 /*concatenacion 1*/
+function exerciseFive() {
+  let resultado2 = "" + numero + numero10;
+  document.write(resultado2 + "<br>");
+}
+exerciseFive();
 
-let numero1 = 13;
-let numero2 = 34;
+function exerciseSix() {
+  let nombre = "josimar leon";
+  let frase = `soy ${nombre} gamer offline`;
+  let frase2 = "mi nombre es 'josimar' y soy un tigre";
+  document.write(frase + "<br>");
+  document.write(frase2 + "<br>");
+}
+exerciseSix();
+/**
+ * * operadores logicos: el = asigna no compara,  ==,===,!=
+ */
 
-resultado = ""+numero1+numero2;
+function exerciseSeven() {
+  let numero4 = 12;
+  let numero5 = 23;
 
-/*concatenacion 2*/
+  document.write((numero4 != numero5) + "<br>");
+}
 
-let nombre = "josimar leon";
-let frase = `soy ${nombre} gamer offline`;
-let frase2 = "mi nombre es 'josimar' y soy un tigre"
+exerciseSeven();
 
+function exerciseEight() {
+  let valor = true;
+  let valor2 = true;
 
-//document.write(frase2);
+  let result = valor && valor2;
+  let result2 = valor || valor2;
+  let result3 = !valor;
 
+  document.write(result+ "<br>");
+  document.write(result2+ "<br>");
+  document.write(result3+ "<br>");
+}
+exerciseEight();
 
-/*operadores logicos: el = asigna no compara,  ==,===,!=,*/
-let numero4 = 12;
-let numero5 = 23;
-
-//document.write(numero4!=numero5);
-
-let valor = true;
-let valor2 = true;
-
-let result = valor && valor2;
-
-let result2 = valor || valor2;
-
-let result3 = !valor;
-
-document.write(result3);
-
-//document.write(frase2);
-
-/* exercise*/
+/**
+ * TODO: Exercise
+ */
 
 let num1 = 12;
 let num2 = 24;
@@ -63,6 +99,6 @@ let num3 = 25;
 let num4 = 92;
 let num5 = 91;
 
-let op=(num1<num2 || num2<num3)&&(!num1&&num5 !=num3);
+let op = (num1 < num2 || num2 < num3) && !num1 && num5 != num3;
 
 document.write(op);
