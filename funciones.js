3c4a4f782cdb7628a3d3822ea9741525a09c2542
josimar <josimarsofive@gmail.com>
2022-09-09 11:18:06 -0500

    modified methods the topics funciones.js

--- funciones.js
+++ funciones.js
@@ -1,36 +1,42 @@
-/** funciones */
+/**
+ * *    Funciones
+ */
 function saludar() {
     return 'todo oka';
 }
 
 let saludo = saludar();
 
-document.write(saludo);
+document.write(saludo+'<br>');
 
-/** funciones - parametros*/
+/**
+ * * funciones - parametros
+ */
 
 function suma(num1,num2) {
     let res = num1+num2;
-    document.write(res);
-    document.write('<br>');
+    document.write(res+'<br>');
 }
 
 suma(12,34);
 suma(62,34);
 
-/** funciones - parametros, flecha*/
-
+/**
+ * * funciones - flecha
+ */
 const saludare = function(nombre) {
     let frase = `!Hola ${nombre} como estas?`;
-    document.write(frase);
+    document.write(frase+'<br>');
 }
+saludare("josimar");
 
 const saludare2 = (nombre) =>{
     let frase = `!Hola ${nombre} como estas bro?`;
-    document.write(frase);
+    document.write(frase+'<br>');
 }
+saludare2("josimar");
 
-const saludare3 = nombre => document.write('hola')
+const saludare3 = nombre => document.write('hola ' + nombre)
 saludare3("pedro");
 
 
