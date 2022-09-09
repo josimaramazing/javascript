1e4209836fbfc545de5f36581ab2ecd4e9734f38
josimar <josimarsofive@gmail.com>
2022-09-09 11:29:01 -0500

    modified methods the topics condicionales.js

--- condicionales.js
+++ condicionales.js
@@ -1,19 +1,27 @@
-/*Condicionales*/ 
+/**
+ * *    Funciones - Bloque de Ejecucion
+ */
 
 {
     alert('Hola Gente');
     document.write('soy josimar')
 }
 
-/*Condicionales - ifelse : si la condicion es true el bloque se ejecuta*/
 
+/**
+ * * Condicionales - ifelse : si la condicion es true el bloque se ejecuta
+ */
 let nombre = 'Pedro';
 
-if (nombre == 'lucky') {
-    alert(`tu nombre es buenardo ${nombre}`)
-} else if(nombre == 'Pepe'){
-    alert(`tu nombre es malardo ${nombre}`)
-}else{
-    alert(`tu nombre es otro`)
+function condicion() {
+    if (nombre == 'lucky') {
+        alert(`tu nombre es buenardo ${nombre}`)
+    } else if(nombre == 'Pepe'){
+        alert(`tu nombre es malardo ${nombre}`)
+    }else{
+        alert(`tu nombre es otro`)
+    }
 }
+
+condicion();
 
